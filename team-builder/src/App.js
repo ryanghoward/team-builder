5d93e06d1a7ce7d41fabe4b3afc075fa7365258d
Ryan Howard <ryanghoward@protonmail.com>
2021-11-09 12:13:02 -0800

    First save

---
+++ team-builder/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const initialFormValues = {
+  username: "",
+  email: "",
+  role: "",
+};
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hello bitch!</h1>
+    </div>
+  );
+}
+
+export default App;
